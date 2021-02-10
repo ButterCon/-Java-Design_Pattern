ff624bb33b9e78ccc5ab4ccb9edbbcbafc56e3e0
진현우 <49802982+jhw3979@users.noreply.github.com>
2021-02-10 23:56:46 +0900

    Builder

--- src/com/butter_con/Builder/Prac01/Computer.java
+++ src/com/butter_con/Builder/Prac01/Computer.java
@@ -2,22 +2,29 @@ package com.butter_con.Builder.Prac01;
 
 
 public class Computer {
-    public Object cpu;
-    public Object[] ram;
-    public Object[] storage;
+    public Object _cpu;
+    public Object[] _ram;
+    public Object[] _storage;
 
     public Computer() {}
 
     public String toString() {
         return
-                "이 컴퓨터의 사양은 CPU=" + this.cpu + ", RAM= " + this.memory() + "GB";
+                "이 컴퓨터의 사양은 CPU=" + this._cpu + ", RAM= " + this.memory() + "GB" + ", Storage= " + this.storage() + "GB 입니다.";
     }
 
     public int memory() {
         int size = 0;
 
-        for (Object mem : this.ram) {
+        for (Object mem : this._ram) {
             size += mem.getSize();
         }
+
+        return 0;
+    }
+
+    public int storage() {
+
+        return 0;
     }
 }
