766b2f9a92d1e770c7ba6f4244afafdd844fab01
진현우 <49802982+jhw3979@users.noreply.github.com>
2021-03-01 23:51:12 +0900

    Prototype

--- src/com/butter_con/Main.java
+++ src/com/butter_con/Main.java
@@ -1,6 +1,6 @@
 package com.butter_con;
 
-import com.butter_con.Builder.Index;
+import com.butter_con.Prototype.Index;
 
 public class Main {
 
---
+++ src/com/butter_con/Prototype/Index.java
@@ -0,0 +1,7 @@
+package com.butter_con.Prototype;
+
+public class Index {
+    public void run() {
+
+    }
+}
